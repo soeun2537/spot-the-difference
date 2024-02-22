fea612c9b3145d01dcbceef14b664b58ef2e9ff3
soeun2537 <soeun2537@gmail.com>
2024-02-23 03:56:21 +0900

    feat: constants/index 파일 추가 (uri 관리)

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const ICON_URLS = {
+  clearButton: require("../../assets/images/clearButton.png"),
+  leftArrow: require("../../assets/images/leftArrow.png"),
+  rightArrow: require("../../assets/images/rightArrow.png"),
+};
+
+export const IMAGE_URLS = [
+  require("../../assets/images/pastImage/past1.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+  require("../../assets/images/pastImage/past2.jpg"),
+];
+
+export const AVATAR_URLS = {
+  grandma1: require("../../assets/images/avatarImage/grandma1.png"),
+  grandma2: require("../../assets/images/avatarImage/grandma2.png"),
+  grandpa1: require("../../assets/images/avatarImage/grandpa1.png"),
+  grandpa2: require("../../assets/images/avatarImage/grandpa2.png"),
+};
