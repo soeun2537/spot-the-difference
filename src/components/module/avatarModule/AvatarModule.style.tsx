cfdd648517f41cf6d42b9c4a1b1db6e89ee883a5
soeun2537 <soeun2537@gmail.com>
2024-02-23 03:49:56 +0900

    feat: AvatarModule 컴포넌트 제작

---
+++ src/components/module/avatarModule/AvatarModule.style.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/native";
+
+export const AvatarModuleContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
---
+++ src/components/module/avatarModule/AvatarModule.tsx
@@ -0,0 +1,35 @@
+import * as S from "./AvatarModule.style";
+import IconButton from "../../button/iconButton/IconButton";
+import AvatarImageViewer from "../../ImageViewer/avatarImageViewer/AvatarImageViewer";
+
+export type AvatarModuleProps = {
+  avatarName: string;
+  onLeftArrowClick: () => void;
+  onRightArrowClick: () => void;
+};
+
+const AvatarModule = ({
+  avatarName,
+  onLeftArrowClick,
+  onRightArrowClick,
+}: AvatarModuleProps) => {
+  return (
+    <S.AvatarModuleContainer>
+      <IconButton
+        iconName={"leftArrow"}
+        width={50}
+        height={50}
+        onPress={onLeftArrowClick}
+      />
+      <AvatarImageViewer avatarName={avatarName} />
+      <IconButton
+        iconName={"rightArrow"}
+        width={50}
+        height={50}
+        onPress={onRightArrowClick}
+      />
+    </S.AvatarModuleContainer>
+  );
+};
+
+export default AvatarModule;
