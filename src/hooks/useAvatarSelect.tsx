c0a0b39eafe6a9b0dc8cfe214844878a728e94b5
soeun2537 <soeun2537@gmail.com>
2024-02-23 03:51:43 +0900

    feat: AvatarMoudle 외부 커스텀 훅 처리

---
+++ src/hooks/useAvatarSelect.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { AVATAR_URLS } from "../constants";
+
+export function useAvatarSelect<T>() {
+  const avatarArray = Object.keys(AVATAR_URLS);
+  const [currentAvatarIndex, setCurrentAvatarIndex] = useState(0);
+
+  const handleLeftArrowClick = () => {
+    setCurrentAvatarIndex((prevIndex) =>
+      prevIndex === 0 ? avatarArray.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleRightArrowClick = () => {
+    setCurrentAvatarIndex((prevIndex) =>
+      prevIndex === avatarArray.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const currentAvatarName = avatarArray[currentAvatarIndex];
+
+  return {
+    handleLeftArrowClick,
+    handleRightArrowClick,
+    currentAvatarName,
+  };
+}
